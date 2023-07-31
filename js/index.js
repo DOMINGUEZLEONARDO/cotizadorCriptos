875987ace0730ca5dad36a26c5560fe1e9f7d467
GestionCompras <dominguezleonardo26@gmail.com>
2023-07-31 17:50:13 -0300

    Segunda entrega Proyecto Final

--- js/index.js
+++ js/index.js
@@ -1,59 +1,101 @@
-let importe =  prompt ("Ingrese capital a invertir U$S:");
-let moneda = prompt ("Que Cripto desea? Bitcoin, Ethereum, Tether o Binance Coin").toLowerCase();
-let bitcoin = 30308
-let ethereum = 1877
-let tether = 1.015
-let binanceCoin = 244
+// let importe =  prompt ("Ingrese capital a invertir U$S:");
+// let moneda = prompt ("Que Cripto desea? Bitcoin, Ethereum, Tether o Binance Coin").toLowerCase();
+// // let bitcoin = 30308
+// let ethereum = 1877
+// let tether = 1.015
+// let binanceCoin = 244
+let moneda 
+let importe
 
-if (moneda === "pesos"){
-    alert("Disculpe, no vendemos papel pintado.")
- } else if (moneda !== "bitcoin" && moneda !== "ethereum" && moneda !== "tether" && moneda !== "binance coin"){
-    alert("No contamos con el activo que usted desea.")
+function Monedas (nombre, valor){
+    this.nombre = nombre;
+    this.valor = valor;
 }
+let bitcoin = new Monedas ('bitcoin',30308);
+let ethereum = new Monedas ('ethereum',1877);
+let tether = new Monedas('tether', 1015);
+let binanceCoin = new Monedas ('binance Coin', 244);
+
 
 function calculador (importe, moneda) {
 
     switch(moneda){
 
         case "bitcoin":
-        return  importe / bitcoin;
+        return  importe / bitcoin.valor;
         break;
 
         case "ethereum":
-        return importe / ethereum;
+        return importe / ethereum.valor;
         break;
 
         case "tether":
-        return importe / tether;
+        return importe / tether.valor;
         break;
 
-        case "binance coin":
-        return importe / binanceCoin;
+        case "binanceCoin":
+        return importe / binanceCoin.valor;
         break;
         
         default:
         return null;
         break;
     }
-    
+        
 }
 
-let shortName; 
+const verificar = ['bitcoin', 'tether','ethereum',"binanceCoin"];
+
+function obtener(){
+    importe = document.getElementById("inversion").value; 
+    moneda = document.getElementById("criptomoneda").value;
+    console.log(verificar.includes(moneda));
+
+   if (moneda === "pesos"){
+        alert("Disculpe, no vendemos papel pintado.")
+     } else if (moneda !== "bitcoin" && moneda !== "ethereum" && moneda !== "tether" && moneda !== "binanceCoin"){
+            alert("No contamos con el activo que usted desea.")
+        }
+        
+
+      
+    let shortName; 
     if (moneda === "bitcoin") {
         shortName = "BTC";
     } else if (moneda === "ethereum") {
         shortName = "ETH";
     } else if (moneda === "tether") {
         shortName = "USDT";
-    } else if (moneda === "binance coin") {
+    } else if (moneda === "binanceCoin") {
         shortName = "BNB";
     } else {
         shortName = "";
     }
 
-if (calculador(importe, moneda) !== null) {
-    console.log("Usted obtendría "+ calculador(importe, moneda).toFixed(8) + " " + shortName);
-} else {
-    console.log("No ingresó un activo válido")
+    
+
+    if (calculador(importe, moneda) !== null) {
+        return  calculador(importe, moneda).toFixed(8) + " " + shortName;
+    } else {
+        return "No ingresó un activo válido"
+    }  
+    
+
 }
+
+
+
+
+    const boton = document.getElementById("boton");
+    
+
+
+    
+
+    const resultado = document.getElementById("valor");
+
+   boton. addEventListener('click',() => {
+    resultado.innerHTML= obtener();
+   }
+   )
 
